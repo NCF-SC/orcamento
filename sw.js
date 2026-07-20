49949739a258e6dd858da0b65c464d5fdc0ce092
NCF-SC <285279116+NCF-SC@users.noreply.github.com>
2026-07-20 20:41:58 -0300

    Manter sessão desbloqueada ao atualizar a página.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "orcamento-v7";
+const CACHE = "orcamento-v8";
 const ASSETS = ["./manifest.json", "./icon.svg", "./restore-pack.json"];
 
 self.addEventListener("install", (event) => {
